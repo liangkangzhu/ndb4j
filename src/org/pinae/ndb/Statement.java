836eb8a73637009123f3af31374c0dfb731f6440
Yugeng Hui <interhuiyg@163.com>
2014-11-27 17:05:51 +0800

    将Apache Common Lang支持更改为Apache Common Lang3支持

--- src/org/pinae/ndb/Statement.java
+++ src/org/pinae/ndb/Statement.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.pinae.ndb.action.Action;
 import org.pinae.ndb.action.OperationAction;
 import org.pinae.ndb.action.TraversalAction;
